d5f7c6443ee1fa9e3b45f3ec950147aa54e9c150
Yaroslav Konyshev <y.konyshev@gmail.com>
2018-11-13 15:19:39 +0100

    some settings + draft.sql annotated

--- IntProj/draft.sql
+++ IntProj/draft.sql
@@ -1,1 +1,21 @@
-#SELECT * FROM pg_catalog.pg_tables;
+/* creation of tables */
+/*
+CREATE TABLE sample_submission
+(
+  SK_ID_CURR character varying(10),
+  TARGET character varying(10)
+)
+*/
+
+/* import */
+--copy sample_submission(sk_id_curr,target) 
+--from '/home/jovyan/ecam_ds/data/input/sample_submission.csv' DELIMITER ',' CSV HEADER;
+
+/* cleanup */
+--drop table sample_submission;
+
+/* test */
+--select count(target) from sample_submission
+
+/* export */
+--COPY sample_submission TO '/home/jovyan/ecam_ds/data/sample_submission_db.csv' DELIMITER ',' CSV HEADER;
