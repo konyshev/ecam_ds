887a87724a8072b582faacd432787090daee8666
Yaroslav Konyshev <y.konyshev@gmail.com>
2018-11-20 10:14:32 +0100

    ordnung + tables are filled

---
+++ IntProj/cleanup.sql
@@ -0,0 +1,44 @@
+/*
+ * 
+ * Cleanup of functions, procedures, tables related to the project
+ * 
+ */
+
+
+CREATE OR REPLACE PROCEDURE yk_cleanup()
+AS $$
+DECLARE
+name_to_drop text;
+cur refcursor;
+
+req text;
+BEGIN
+	req :='SELECT routine_name 
+	  		 FROM information_schema.routines 
+			WHERE routines.specific_schema=''public''';
+
+	OPEN cur FOR EXECUTE(req); 
+	LOOP 
+	   	FETCH cur INTO name_to_drop;
+	    EXIT WHEN NOT FOUND;
+	   EXECUTE format('DROP PROCEDURE IF EXISTS %s ;', name_to_drop);	  		
+	END LOOP;
+	CLOSE cur;
+
+	req := 'SELECT table_name
+  			  FROM information_schema.tables 
+			 WHERE table_schema=''public'' and table_name not in (''albums'',''artistes'',''chansons'')';
+
+	OPEN cur FOR EXECUTE(req); 
+	LOOP 
+	   	FETCH cur INTO name_to_drop;
+	    EXIT WHEN NOT FOUND;
+	   	EXECUTE format('DROP TABLE IF EXISTS %s ;', name_to_drop);	  		
+	END LOOP;
+	CLOSE cur;
+
+END;
+$$ LANGUAGE plpgsql;
+
+/* cleanup */
+call yk_cleanup();
--- IntProj/create_tables.sql
+++ IntProj/create_tables.sql
@@ -8,10 +8,10 @@
  * Problems:
  * 1. table previous application fields were contained extra spaces in name and did not matched
  * 2. needed to fix application{train|test}.csv case
+ * 3. extra column in description
+ * 4. remove .csv
+ * 5. TARGET doesn't in test
  * */
-
-/* cleanup */
-call yk_cleanup();
 
 /* create initial table with structure of tables */
 CREATE TABLE IF NOT EXISTS yk_data_struct
@@ -26,71 +26,61 @@ special TEXT
 /* fill the table */
 CALL yk_csv_to_table('yk_data_struct',
 					 '/home/jovyan/ecam_ds/data/input/HomeCredit_columns_description.csv');
-
-/* check table*/
-select * from yk_data_struct limit 5; 
---delete from yk_data_struct;
-
 					
 /* Rename 'application_{train|test}.csv' case */
-update 
-   yk_data_struct
-set
-   table_name = REPLACE (
-   table_name,
-   'application_{train|test}.csv',
-   'application_train.csv'
-   )
-where table_name = 'application_{train|test}.csv';
+update yk_data_struct
+   set
+      table_name = REPLACE (
+      table_name,
+      'application_{train|test}.csv',
+      'application_train.csv'
+      )
+ where table_name = 'application_{train|test}.csv';
+COMMIT;
 
 /* check if case is fixed */
-select distinct table_name 
-  from yk_data_struct 
- where table_name ~ '^appl';
+--select distinct table_name 
+--  from yk_data_struct 
+-- where table_name ~ '^appl';
 
-/* remove .csv to upper case */
+/* remove .csv from table_name and spaces from column_name */
 update yk_data_struct
-set
-   table_name = REPLACE (
-   table_name,
- '.csv',
- ''
-   );
-
-/* remove spaces from column names */
-update yk_data_struct
-set
-   column_name = REPLACE (
-   column_name,
- ' ',
- ''
-   );
+   set
+      table_name = REPLACE (
+         table_name,
+         '.csv',
+         ''
+      ),
+      column_name = REPLACE (
+         column_name,
+         ' ',
+         ''
+      )
+;
+COMMIT;
 
 /* column does not exists in previous_application.csv */
-delete from yk_data_struct where column_name = 'NFLAG_MICRO_CASH' and table_name = 'previous_application';
+delete from yk_data_struct 
+ where column_name = 'NFLAG_MICRO_CASH' 
+   and table_name = 'previous_application';
+COMMIT;
   
 /* setup correct sequence number */
-select setval('yk_data_struct_id_seq',  (select max(id) from yk_data_struct));
+select  setval('yk_data_struct_id_seq',  (select max(id) from yk_data_struct));
 
 /* add records for APPLICATION_TEST table */
 insert into yk_data_struct 
-select nextval('yk_data_struct_id_seq') as id,
-	   'application_test' as table_name,
-	   column_name,
-	   column_description,
-	   special
-from yk_data_struct where table_name = 'application_train' and column_name <> 'TARGET';
+	select nextval('yk_data_struct_id_seq') as id,
+		   'application_test' as table_name,
+		   column_name,
+		   column_description,
+		   special
+	from yk_data_struct 
+   where table_name = 'application_train' 
+	 and column_name <> 'TARGET';
+COMMIT;
 
-/*
- * 
- * Creation of tables based on data structure table
- * 
- * */
-
-select distinct table_name from yk_data_struct order by 1;
-
+--Creation of tables based on data structure table 
 CALL yk_create_tables('yk_data_struct');
 
 CALL yk_fill_tables('yk_data_struct','/home/jovyan/ecam_ds/data/input/');
-
-CALL yk_csv_to_table('POS_CASH_balance','/home/jovyan/ecam_ds/data/input/POS_CASH_balance.csv');
