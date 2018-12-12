ad5e2b75447103a6cfb7600c4abf0668e07a1f5a
Yaroslav Konyshev <y.konyshev@gmail.com>
2018-12-12 14:54:02 +0100

    ex01 changes

--- exercices/Java/ex01/src/main/java/com/ecam/Main.java
+++ exercices/Java/ex01/src/main/java/com/ecam/Main.java
@@ -1,22 +1,55 @@
 package com.ecam;
 
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner inputFromUser = new Scanner(System.in);
-        System.out.println("Enter X:"); 
-        int xFromUser = inputFromUser.nextInt();
+        Vector<Point> points = new Vector<Point>(10);
 
-        System.out.println("Enter Y:"); 
-        int yFromUser = inputFromUser.nextInt();
+        String[] names = {"A","B","C","D","E","F","G","H","I","J"};
+        int[] abscissa = {1,2,4,-2,-3,-3,5,6,0,0};
+        int[] ordinate = {3,0,3,3,1,4,-3,-1,2,5};
 
-        inputFromUser.close();
+        for(int i = 0; i< names.length ;i++){
+            points.add(new Point(names[i], abscissa[i], ordinate[i]));
+        }
+        
+        for(Point p:points){
+            System.out.println(p.info());
+        }
 
-        Point p = new Point();
-        p.setX(xFromUser);
-        p.setY(yFromUser);
-        System.out.println("X = "+p.getX()); // Display the string.
-        System.out.println("Y = "+p.getY()); // Display the string.
+//        Scanner inputFromUser = new Scanner(System.in);
+//        System.out.println("Enter a point name:"); 
+//        String nameFromUser = inputFromUser.nextLine();
+        int numberOfPoints = 0;
+   
+        for(Point p:points){
+//            if (p.getName().equals(nameFromUser)){
+//                System.out.println(" User wanted --> "+p.info());
+//            }
+            if (p.getX()==0 || p.getY()==0)
+            {
+                System.out.println(" X or Y is zero --> "+p.info());
+                numberOfPoints++;
+            }            
+        }
+
+        System.out.println(numberOfPoints + " point(s) with zero coordinate");
+/*
+        if(points.elementAt(0).equals(points.elementAt(9))){
+            System.out.println(" equal! ");
+        }
+        else {
+            System.out.println(" not equal! ");
+        }
+*/
+        Collections.sort(points);
+
+        for(Point p:points){
+            System.out.println(p.info());
+        }
+
+//        inputFromUser.close();
     }
 }
--- exercices/Java/ex01/src/main/java/com/ecam/Point.java
+++ exercices/Java/ex01/src/main/java/com/ecam/Point.java
@@ -1,7 +1,34 @@
 package com.ecam;
+import java.lang.*;
 
-public class Point {
+public class Point implements Comparable {
+
+    @Override
+    public int compareTo(Object o) {
+        if (o instanceof Point){
+            Point p = (Point) o;
+            return Integer.compare(this.getX(), p.getX());
+        } else {
+            return -1;
+        }
+    }
+
     private int x,y;
+    private String name;
+
+    public Point(String name, int x,int y){
+        setX(x);
+        setY(y);
+        setName(name);
+    }
+
+    public void setName(String newValue) {
+        this.name = newValue;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
     public void setX(int newValue) {
         this.x = newValue;
@@ -18,4 +45,17 @@ public class Point {
     public int getY() {
         return this.y;
     }
+
+    public String info() {
+        return this.getName()+":("+this.getX()+","+this.getY()+")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Point point = (Point) o;
+        return x==point.x &&
+                y==point.y &&
+                 java.util.Objects.equals(name,point.name);
+    }
+
 }
